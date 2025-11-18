d9bd0f545c1cc84daafd535afc917f7eb33f6e8b
Aishani-123 <aishanisingh232@gmail.com>
2025-11-18 19:25:34 +0530

    Add files via upload

--- settle_pay.c
+++ settle_pay.c
@@ -1,4 +1,4 @@
-struct Expense {
+/*struct Expense {
     char groupName[20];
     char category[20];
     char payer[20];
@@ -46,4 +46,67 @@ void settle(const char *groupName, const char *payer, const char *receiver) {
         printf("Settlement successful.\n");
     else
         printf(" No unpaid expense found.\n");
+    strcat(e.date, " (Settled)");
+}*/
+struct Expense {
+    char groupName[20];
+    char category[20];
+    char payer[20];
+    char participants[20];
+    float amount;
+    char date[10];
+};
+void settle(const char *groupName, const char *payer, const char *receiver) {
+    struct Expense e;
+    FILE *fp = fopen("expenses.txt", "r");  
+    if (!fp) {
+        fp = fopen("expenses.txt", "a+");
+        if (!fp) {
+            printf("Error creating expense file.\n");
+            return;
+        }
+    }
+    FILE *temp = fopen("temp.txt", "w");   
+    int found = 0;
+    if (!fp || !temp) {
+        printf("Error opening expense files.\n");
+        return;
+    }
+    FILE *h = fopen("hist.txt", "a+");  
+    if (!h) {
+        printf("Error opening history file.\n");
+        fclose(fp);
+        fclose(temp);
+        return;
+    }
+    while (fscanf(fp, "%19s %19s %19s %19s %f %9[^\n]",
+                  e.groupName, e.category, e.payer,
+                  e.participants, &e.amount, e.date) == 6) {
+         if (strcmp(e.groupName, groupName) == 0 &&
+            strstr(e.participants, receiver) != NULL &&
+            strcmp(e.payer, payer) == 0) {
+            found = 1;
+            fprintf(h, "%s %s %s %s %.2f %s\n",
+                    e.groupName, e.category, e.payer,
+                    e.participants, e.amount, e.date);
+        } else {
+            fprintf(temp, "%s %s %s %s %.2f %s\n",
+                    e.groupName, e.category, e.payer,
+                    e.participants, e.amount, e.date);
+        } }
+    fclose(fp);
+    fclose(temp);
+    fclose(h);
+    remove("expenses.txt");
+    rename("temp.txt", "expenses.txt");
+    if (found)
+        printf("Settlement successful.\n");
+    else
+        printf("No unpaid expense found.\n");
 }
+void transitive(const char *groupName, const char *payer, const char *middle, const char *receiver) {
+    settle(groupName, payer, middle);
+    settle(groupName, middle, receiver);
+    printf("Transitive settlement complete.\n");
+}
+
